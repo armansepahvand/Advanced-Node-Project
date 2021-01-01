92b1f4cf99121ac878c2093ac0a31f4e4376a339
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 16:01:33 -0700

    added a afterEach statement to close the browser after each test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 let browser, page;
 
+//Initiate the browser befiore each test
 beforeEach(async () => {
   browser = await puppeteer.launch({
     headless: false,
@@ -8,6 +9,11 @@ beforeEach(async () => {
   page = await browser.newPage();
   //open the main page of the app
   await page.goto("localhost:3000");
+});
+
+//close browser afgter each test
+afterEach(async () => {
+  await browser.close();
 });
 
 test("we can launch a browser", async () => {
