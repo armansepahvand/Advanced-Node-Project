3afd238adf599c7efaebed8cd014f13a4f7fa016
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 00:32:01 -0700

    formatting

--- services/cache.js
+++ services/cache.js
@@ -1,24 +1,26 @@
 const mongoose = require("mongoose");
-const util=require('util');
+const util = require("util");
 
 //set up the redis client
-const redis= require('redis');
-const redisUrl="redis://127.0.0.1:6379";
-const client =redis.createClient(redisUrl);
+const redis = require("redis");
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
 
 //promisify client.get to return a promise
-client.get=util.promisify(client.get);
+client.get = util.promisify(client.get);
 
 //save the mongoose query exec prototype function into a constant
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
+  //Create a unique key from mongoose collection name and query options
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
 
-  //Create a unique key from mongoose collection name and query options  
-  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name,
-  });
-  
+ 
 
   return exec.apply(this, arguments);
 };
