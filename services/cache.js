d520233c72646dc3bc667a68f8d19083f5ae7d2a
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-31 23:55:43 -0700

    saved the mongoose query exec prototype function into a constant

--- services/cache.js
+++ services/cache.js
@@ -1,1 +1,4 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
+
+//save the mongoose query exec prototype function into a constant
+const exec = mongoose.Query.prototype.exec;
