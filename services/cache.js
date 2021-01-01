812562b1175fded6490f5f895b5aa678aea9e432
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 00:23:01 -0700

    set up the redis client

--- services/cache.js
+++ services/cache.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+//set up the redis client
+const redis= require('redis');
+const redisUrl="redis://127.0.0.1:6379";
+const client =redis.createClient(redisUrl);
 
 //save the mongoose query exec prototype function into a constant
 const exec = mongoose.Query.prototype.exec;
