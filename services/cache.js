df9e4ba44f918c3f84f4a547ee93bedf5f41e254
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 00:01:33 -0700

    added a override structure to add cache logic inside mongoose exec function

--- services/cache.js
+++ services/cache.js
@@ -2,3 +2,9 @@ const mongoose = require("mongoose");
 
 //save the mongoose query exec prototype function into a constant
 const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function () {
+  
+
+  return exec.apply(this, arguments);
+};
