c1bd0ab55846cba1b68f7e8d6d21b934f6eec2ed
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 15:55:25 -0700

    refactor the code to use a beforeEach statement to open the browser for each test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,14 +1,16 @@
 const puppeteer = require("puppeteer");
+let browser, page;
 
-test("we can launch a browser", async () => {
-  const browser = await puppeteer.launch({
+beforeEach(async () => {
+  browser = await puppeteer.launch({
     headless: false,
   });
-  const page = await browser.newPage();
-
+  page = await browser.newPage();
   //open the main page of the app
   await page.goto("localhost:3000");
+});
 
+test("we can launch a browser", async () => {
   //get the text from an a tag inside of the header
   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
 
