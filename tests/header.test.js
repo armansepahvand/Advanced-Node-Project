bfbab26203327e8cecce334e2ed0af411bd8f15b
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 15:04:38 -0700

    get the text from an a tag inside of the header

--- tests/header.test.js
+++ tests/header.test.js
@@ -7,4 +7,6 @@ test("we can launch a browser", async () => {
   const page = await browser.newPage();
   //open the main page of the app
   await page.goto("localhost:3000");
+  //get the text from an a tag insite of the header
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
 });
