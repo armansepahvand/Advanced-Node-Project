1278c46a7e6f40609e1755cd35e50bdc73078d27
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 00:18:20 -0700

    Created a unique key from mongoose collection name and query options

--- services/cache.js
+++ services/cache.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose");
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
+
+  //Create a unique key from mongoose collection name and query options  
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
   
 
   return exec.apply(this, arguments);
