c52e561e16f99b36f8414ee01faae34205e9bc16
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 00:24:27 -0700

    promisify client.get to return a promise

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
+const util=require('util');
+
 //set up the redis client
 const redis= require('redis');
 const redisUrl="redis://127.0.0.1:6379";
 const client =redis.createClient(redisUrl);
+
+//promisify client.get to return a promise
+client.get=util.promisify(client.get);
 
 //save the mongoose query exec prototype function into a constant
 const exec = mongoose.Query.prototype.exec;
