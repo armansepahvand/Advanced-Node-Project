872b75c380f87ba18793451a657570e7107da00b
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 15:05:14 -0700

    make assertion for the text

--- tests/header.test.js
+++ tests/header.test.js
@@ -5,8 +5,13 @@ test("we can launch a browser", async () => {
     headless: false,
   });
   const page = await browser.newPage();
+
   //open the main page of the app
   await page.goto("localhost:3000");
-  //get the text from an a tag insite of the header
+
+  //get the text from an a tag inside of the header
   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+
+  //make assertion for the text
+  expect(text).toEqual("Blogster");
 });
