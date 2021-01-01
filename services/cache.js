b5673fe27e694c236ff49ca43b0853979a94b8a1
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 02:33:59 -0700

    Changed the redis key to hashkey to create a nested key

--- services/cache.js
+++ services/cache.js
@@ -7,20 +7,22 @@ const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
 
 //promisify client.get to return a promise
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 //save the mongoose query exec prototype function into a constant
 const exec = mongoose.Query.prototype.exec;
 
 //prototype function to set cache to true for each query that applies this function
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
   return this;
 };
 
 mongoose.Query.prototype.exec = async function () {
   //if the the route does not aplly the cache function return run the original exec function
   if (!this.useCache) {
+    console.log("no cache");
     return exec.apply(this, arguments);
   }
   //Create a unique key from mongoose collection name and query options
@@ -31,10 +33,11 @@ mongoose.Query.prototype.exec = async function () {
   );
 
   //save the value for the key in cacheValue if it exists
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   //return the value if it exists
   if (cacheValue) {
+    console.log("Cache used");
     const doc = JSON.parse(cacheValue);
 
     return Array.isArray(doc)
@@ -46,6 +49,6 @@ mongoose.Query.prototype.exec = async function () {
   const result = await exec.apply(this, arguments);
 
   //set the result of mongoose query exec into redis and set the expiery to 10 second
-  client.set(key, JSON.stringify(result), "EX", 10);
+  client.hset(this.hashKey, key, JSON.stringify(result));
   return result;
 };
