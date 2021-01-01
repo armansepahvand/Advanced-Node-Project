1453915f55a351afda8c7d4195515d4fffd9a006
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-01 02:36:11 -0700

    cleaned the code

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -15,13 +15,9 @@ module.exports = (app) => {
   });
 
   app.get("/api/blogs", requireLogin, async (req, res) => {
-    const redis = require('redis');
-    const redisUrl='redis://127.0.0.1:6379';
-    const client = redis.createClient(redisUrl);
-
-
-
-    const blogs = await Blog.find({ _user: req.user.id });
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id,
+    });
 
     res.send(blogs);
   });
