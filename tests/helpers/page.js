3f3cb9cac87be9b41f261632077dd70a512880b7
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-03 19:43:45 -0700

    Comments added

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -2,13 +2,19 @@ const puppeteer = require("puppeteer");
 
 class CustomPage {
   static async build() {
+    //create a browser instance
     const browser = await puppeteer.launch({
       headless: false,
     });
 
+    //create a page instance from
     const page = await browser.newPage();
+
+    //create a ne custumePage object
     const custumPage = new CustomPage(page);
 
+    // a proxy to return the proper function from one of the browser, page or
+    //custompage functions using proxy method
     return new Proxy(customPage, {
       get: function (target, property) {
         return customPage[property] || page[property] || browser[property];
